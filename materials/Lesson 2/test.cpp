2c0543fcb4fff23bdb0dc1e1601260048e70d6eb
Jerry Treeman <35148370+TieBeTie@users.noreply.github.com>
2023-03-26 06:59:34 +0300

    Lesson 2

--- materials/Lesson 2/test.cpp
+++ materials/Lesson 2/test.cpp
@@ -2,8 +2,26 @@
 
 #include "Queue.hpp"
 
+using std::cout;
+
 int main() {
   { Queue<int> a; }
-  Queue<double> b;
+  Queue<double> b(1);
+  b.PushFront(1);
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
+  b.PushFront(2);
+  b.PopBack();
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
+  b.PushFront(3);
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
+  b.PushFront(4);
+  b.PopBack();
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
+  b.PushFront(5);
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
+  b.PopBack();
+  b.PopBack();
+  b.PushFront(6);
+  cout << b.Front() << ' ' << b.Back() << ' ' << b.Size() << '\n';
   std::cout << -2 % 5;
 }
